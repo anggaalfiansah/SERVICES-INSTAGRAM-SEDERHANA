6c6f4ce1dc1d23c047c13aa0a0324ccb33d7db8e
anggaalfiansah <anggaalfiansah26@gmail.com>
2021-06-19 17:13:06 +0700

    first commit

---
+++ api/index.js
@@ -0,0 +1,140 @@
+const { IgApiClient, IgCheckpointError } = require("instagram-private-api");
+var { get } = require("request-promise");
+const sharp = require("sharp");
+
+exports.uploadFeedAPI = async (
+  username,
+  password,
+  picture,
+  namaProduk,
+  harga,
+  variant,
+  cb
+) => {
+  console.log(picture);
+  let variantList = "";
+  await Promise.all(
+    variant.map(async (item) => {
+      variantList += `-${item}\r\n`;
+    })
+  );
+  const caption = `OPEN ORDER !!!\r\nNama Produk: ${namaProduk}\r\nHarga: ${harga}\r\nVarian:\r\n${variantList}Silahkan diorder`;
+  const ig = new IgApiClient();
+  ig.state.generateDevice(username);
+  const auth = await ig.account
+    .login(username, password)
+    .catch(() => cb("username/password salah"));
+  if (auth) {
+    const albumItem = [];
+    let gambarError = 0;
+    await Promise.all(
+      picture.map(async (item) => {
+        const imageBuffer = await get({
+          url: item,
+          encoding: null, // this is required, only this way a Buffer is returned
+        }).catch(async () => {
+          await gambarError++;
+          console.log("url tidak valid");
+        });
+        if (imageBuffer) {
+          const gambar = await sharp(imageBuffer)
+            .resize({ height: 800, width: 800 })
+            .toFormat("jpeg")
+            .toBuffer();
+          await albumItem.push({ file: gambar, height: 800, width: 800 });
+        }
+      })
+    );
+    console.log(albumItem);
+    if (albumItem.length > 1) {
+      try {
+        const publish = await ig.publish.album({
+          items: albumItem, // image buffer, you also can specify image from your disk using fs
+          caption: caption, // nice caption (optional)
+        });
+        const logout = await ig.account.logout();
+        console.log("Status Logout " + JSON.stringify(logout));
+        cb({data : publish, });
+      } catch (error) {
+        const logout = await ig.account.logout();
+        console.log("Status Logout " + JSON.stringify(logout));
+        console.log(error);
+        cb("error");
+      }
+    } else {
+      const logout = await ig.account.logout();
+      console.log("Status Logout " + JSON.stringify(logout));
+      cb({ message: "gambar kurang, gambar error " + gambarError });
+    }
+  }
+};
+
+exports.uploadStoryAPI = async (username, password, picture, cb) => {
+  try {
+    const ig = new IgApiClient();
+    ig.state.generateDevice(username);
+    const auth = await ig.account
+      .login(username, password)
+      .catch(() => cb("username/password salah"));
+    if (auth) {
+      const imageBuffer = await get({
+        url: picture,
+        encoding: null, // this is required, only this way a Buffer is returned
+      });
+      const gambar = await sharp(imageBuffer)
+        .resize({ height: 800, width: 800 })
+        .toFormat("jpeg")
+        .toBuffer();
+      const launch = await ig.publish.story({ file: gambar });
+      const logout = await ig.account.logout();
+      console.log("Status Logout " + JSON.stringify(logout));
+      cb(launch);
+    }
+  } catch (error) {
+    console.log(error);
+    cb("error");
+  }
+};
+
+exports.uploadFeedWeb = async (username, password, picture, caption, cb) => {
+  try {
+    const ig = new IgApiClient();
+    ig.state.generateDevice(username);
+    const auth = await ig.account.login(username, password);
+    console.log(auth);
+    if (auth) {
+      await Promise.all(
+        picture.map(async (item) => {
+          if (item !== "") {
+            const imageBuffer = await get({
+              url: item,
+              encoding: null, // this is required, only this way a Buffer is returned
+            });
+            const gambar = await sharp(imageBuffer)
+              .resize({ height: 800, width: 800 })
+              .toFormat("jpeg")
+              .toBuffer();
+            await albumItem.push({ file: gambar, height: 800, width: 800 });
+          }
+        })
+      );
+      console.log(albumItem.length);
+      if (albumItem !== []) {
+        await ig.publish.album({
+          items: albumItem, // image buffer, you also can specify image from your disk using fs
+          caption: caption, // nice caption (optional)
+        });
+        const logout = await ig.account.logout();
+        console.log("Status Logout " + JSON.stringify(logout));
+        await ig.account.logout();
+        cb("sukses");
+      } else {
+        await ig.account.logout();
+        cb("link null");
+      }
+    } else cb("userpass wrong");
+  } catch (error) {
+    console.log(error);
+    cb("error");
+  }
+};
---
+++ routes/Instagram.js
@@ -0,0 +1,113 @@
+var express = require("express");
+const { uploadFeedWeb, uploadFeedAPI, uploadStoryAPI } = require("../api");
+var router = express.Router();
+
+/* Post Feed With Caption */
+router.post("/feed", async (req, res) => {
+  const { username, password, picture, namaProduk, harga, variant } = req.body;
+  if (username !== "" && password !== "") {
+    if (picture && picture.length > 1 && picture !== "") {
+      await uploadFeedAPI(
+        username,
+        password,
+        picture,
+        namaProduk,
+        harga,
+        variant,
+        (upload) => {
+          console.log(upload);
+          switch (upload) {
+            case "username/password salah":
+              res.send({ message: "username/password salah" });
+              break;
+            case "error":
+              res.send({
+                message:
+                  "Terjadi kesalahan harap periksa kembali data yang dikirimkan",
+              });
+              break;
+            default:
+              res.send(upload);
+              break;
+          }
+        }
+      );
+    } else if (picture.length == 1) {
+      res.send({ message: "harap masukan gambar lebih dari 1" });
+    } else res.send({ message: "harap masukan gambar" });
+  } else {
+    res.send({ message: "harap masukan username & password" });
+  }
+});
+
+router.post("/story", async (req, res) => {
+  const { username, password, picture } = req.body;
+  console.log(picture);
+  if (username !== "" && password !== "") {
+    if (picture && picture !== "") {
+      await uploadStoryAPI(username, password, picture, (upload) => {
+        console.log(upload);
+        res.send(upload);
+        switch (upload) {
+          case "username/password salah":
+            res.send({ message: "username/password salah" });
+            break;
+          case "error":
+            res.send({
+              message:
+                "Terjadi kesalahan harap periksa kembali data yang dikirimkan",
+            });
+            break;
+          default:
+            res.send(upload);
+            break;
+        }
+      });
+    } else res.send({ message: "harap masukan link gambar yang benar" });
+  } else {
+    res.send({ message: "harap masukan username & password" });
+  }
+});
+
+router.post("/web/feed", async (req, res) => {
+  const { username, password, picture, caption } = req.body;
+  console.log(req.body);
+  if (username !== "" && password !== "") {
+    await uploadFeedWeb(username, password, picture, caption, (upload) => {
+      console.log(upload);
+      switch (upload) {
+        case "usepass wrong":
+          res.send(
+            `<script>alert("username/password salah");window.location.replace("/")</script>`
+          );
+          break;
+
+        case "link null":
+          res.send(
+            `<script>alert("harap masukan link gambar");window.location.replace("/")</script>`
+          );
+          break;
+        case "sukses":
+          res.send(
+            `<script>alert("Upload Sukses");window.location.replace("/")</script>`
+          );
+          break;
+
+        case "error":
+          res.send(
+            `<script>alert("Upload Error atau username/password salah");window.location.replace("/")</script>`
+          );
+          break;
+
+        default:
+          break;
+      }
+    });
+  } else {
+    res.send(
+      `<script>alert("masukan username/password");window.location.replace("/")</script>`
+    );
+  }
+});
+
+module.exports = router;
